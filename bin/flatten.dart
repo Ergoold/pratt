a4240e9a2651d9b24ffd1bc04660e01d423f3692
Ergoold <eran1gold@gmail.com>
2020-09-11 18:58:05 +0300

    reformat flatten

--- bin/flatten.dart
+++ bin/flatten.dart
@@ -9,7 +9,8 @@ Tree flatten(Tree tree) {
       if (tree.children.length == 1) return tree;
       for (var i = 0; i < tree.children.length; i++) {
         if (tree.children[0].root is! Operator) break;
-        if ((tree.children[0].root as Operator).value == (tree.root as Operator).value) {
+        if ((tree.children[0].root as Operator).value ==
+            (tree.root as Operator).value) {
           var children = tree.children[0].children;
           tree.children.removeAt(0);
           tree.children.insertAll(0, children);
