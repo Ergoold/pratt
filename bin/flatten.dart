cd4569c4ce6fd10b763afc7a5db13d9ce92fd5a6
Ergoold <eran1gold@gmail.com>
2020-09-11 18:12:56 +0300

    fix `flatten`

--- bin/flatten.dart
+++ bin/flatten.dart
@@ -2,13 +2,14 @@ import 'token.dart';
 import 'tree.dart';
 
 Tree flatten(Tree tree) {
-  if (tree.root.type == Type.atom) return tree;
-  switch (tree.root.value) {
+  if (tree.root is Atom) return tree;
+  switch ((tree.root as Operator).value) {
     case '+':
     case '*':
       if (tree.children.length == 1) return tree;
       for (var i = 0; i < tree.children.length; i++) {
-        if (tree.children[0].root.value == tree.root.value) {
+        if (tree is! Operator) break;
+        if ((tree.children[0].root as Operator).value == (tree.root as Operator).value) {
           var children = tree.children[0].children;
           tree.children.removeAt(0);
           tree.children.insertAll(0, children);
